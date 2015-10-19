4a31b3e705dd9eb516395e0214d1565cd73b0db6
Anderson Jo <a141890@gmail.com>
2015-10-19 15:42:00 +0900

    ADD matrix

--- 320_matrix/test.py
+++ 320_matrix/test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 import numpy as np
-from yourtest import matrix_add, matrix_multiply
+from yourtest import matrix_add, matrix_multiply, matrix_scalar_multiply
 
 
 class MatrixTest(TestCase):
@@ -30,10 +30,10 @@ class MatrixTest(TestCase):
         c = np.matrix(self.data3)
         d = np.matrix(self.data4)
 
-        self.assertEqual(True, (a * 3 == matrix_multiply(self.data1, 3)).all())
-        self.assertEqual(True, (b * -1 == matrix_multiply(self.data2, -1)).all())
-        self.assertEqual(True, (c * 0 == matrix_multiply(self.data3, 0)).all())
-        self.assertEqual(True, (d * 0.4 == matrix_multiply(0.4, self.data4)).all())
+        self.assertEqual(True, (a * 3 == matrix_scalar_multiply(self.data1, 3)).all())
+        self.assertEqual(True, (b * -1 == matrix_scalar_multiply(self.data2, -1)).all())
+        self.assertEqual(True, (c * 0 == matrix_scalar_multiply(self.data3, 0)).all())
+        self.assertEqual(True, (d * 0.4 == matrix_scalar_multiply(0.4, self.data4)).all())
 
     def test_multiplication(self):
         a = np.matrix(self.data1)
@@ -41,5 +41,7 @@ class MatrixTest(TestCase):
         c = np.matrix(self.data3)
         d = np.matrix(self.data4)
 
-        print a * b.T
-        print a.T.tolist()
+        self.assertEqual(True, (a * b.T == matrix_multiply(a.tolist(), b.T.tolist())).all())
+        self.assertEqual(True, (a.T * b == matrix_multiply(a.T.tolist(), b.tolist())).all())
+        self.assertEqual(True, (b * a.T == matrix_multiply(b.tolist(), a.T.tolist())).all())
+        self.assertEqual(True, (b.T * a == matrix_multiply(b.T.tolist(), a.tolist())).all())
